ec87ef212cf86308597fbd95e0b2d9a9b32c945d
Omar Morales Demetrio <20203tn096@utez.edu.mx>
2021-08-10 19:07:51 -0500

    Se integro la creacion de games

--- ServletsAsync_3C/target/ServletsAsync_3C-1.0-SNAPSHOT/assets/dist/js/main.js
+++ ServletsAsync_3C/target/ServletsAsync_3C-1.0-SNAPSHOT/assets/dist/js/main.js
@@ -9,17 +9,31 @@ function findGames(){
         result.innerHTML='';
         var count=0;
         for (let game of listGame) {
-            count=count+1;
-            result.innerHTML += `
+            if(game.status==1) {
+                count = count + 1;
+                result.innerHTML += `
            <tr>
            <td>${count}</td>
            <td>${game.name}</td>
-           <td><img width="100" height="100" src="data:image/jpeg;base64,${ game.imgGame }"</td>
+           <td><img width="100" height="100" src="data:image/jpeg;base64,${game.imgGame}"</td>
            <td>${game.datePremiere}</td>
            <td>${game.Category_idCategory.nameCategory}</td>
-           <td>${game.status}</td>
+           <td><span style="color: green"> Activo</span></td>
            </tr>
            `
+            }else{
+                count = count + 1;
+                result.innerHTML += `
+           <tr>
+           <td>${count}</td>
+           <td>${game.name}</td>
+           <td><img width="100" height="100" src="data:image/jpeg;base64,${game.imgGame}"</td>
+           <td>${game.datePremiere}</td>
+           <td>${game.Category_idCategory.nameCategory}</td>
+           <td><span style="color: red"> Inactivo</span></td>
+           </tr>
+           `
+            }
         }
     });
 }
